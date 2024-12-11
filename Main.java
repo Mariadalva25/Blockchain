51147d8d783e4553d1819aa7359aadf92b697451
Maria Dalva <maria.dalva@aluno.uepb.edu.br>
2024-12-11 09:19:25 -0300

    Adicionando mais funcionalidades

--- Main.java
+++ Main.java
@@ -6,20 +6,23 @@ class Bloco {
     private String hashAnterior;
     private List<String> transacoes;
     protected int nonce;
+    private double recompensaMinerador;
 
     public Bloco(String hashAnterior, List<String> transacoes) {
         this.hashAnterior = hashAnterior;
         this.transacoes = transacoes;
         this.nonce = 0;
         this.hash = calcularHash();
+        this.recompensaMinerador = 0.0; // Inicializando a recompensa do minerador
     }
 
-    public void minerar(int dificuldade) {
+    public void minerar(int dificuldade, double recompensa) {
         String prefixo = new String(new char[dificuldade]).replace('\0', '0');
         while (!calcularHash().substring(0, dificuldade).equals(prefixo)) {
             nonce++;
             hash = calcularHash();
         }
+        this.recompensaMinerador = recompensa; // Atribuindo a recompensa para o minerador
     }
 
     public String calcularHash() {
@@ -49,22 +52,28 @@ class Bloco {
     public String getHashAnterior() {
         return hashAnterior;
     }
+
+    public double getRecompensaMinerador() {
+        return recompensaMinerador;
+    }
 }
 
 class Blockchain {
     private List<Bloco> blocos;
+    private Map<String, Double> saldos;
     private Map<String, List<String>> historicoTransacoes;
 
     public Blockchain() {
         blocos = new ArrayList<>();
-        blocos.add(new Bloco("0", List.of("Bloco Gênese"))); 
+        blocos.add(new Bloco("0", List.of("Bloco Gênese")));
+        saldos = new HashMap<>();
         historicoTransacoes = new HashMap<>();
     }
 
-    public void adicionarBloco(List<String> transacoes, int dificuldade) {
+    public void adicionarBloco(List<String> transacoes, int dificuldade, double recompensaMinerador) {
         Bloco blocoAnterior = blocos.get(blocos.size() - 1);
         Bloco novoBloco = new Bloco(blocoAnterior.getHash(), transacoes);
-        novoBloco.minerar(dificuldade);
+        novoBloco.minerar(dificuldade, recompensaMinerador);
         blocos.add(novoBloco);
 
         for (String transacao : transacoes) {
@@ -72,7 +81,29 @@ class Blockchain {
             String remetente = partes[0].trim();
             historicoTransacoes.putIfAbsent(remetente, new ArrayList<>());
             historicoTransacoes.get(remetente).add(transacao);
+
+            // Atualizar saldos
+            if (saldos.containsKey(remetente)) {
+                double saldoRemetente = saldos.get(remetente);
+                saldoRemetente -= Double.parseDouble(partes[2].trim());
+                saldos.put(remetente, saldoRemetente);
+            } else {
+                saldos.put(remetente, -Double.parseDouble(partes[2].trim()));
+            }
+
+            String destinatario = partes[1].trim();
+            if (saldos.containsKey(destinatario)) {
+                double saldoDestinatario = saldos.get(destinatario);
+                saldoDestinatario += Double.parseDouble(partes[2].trim());
+                saldos.put(destinatario, saldoDestinatario);
+            } else {
+                saldos.put(destinatario, Double.parseDouble(partes[2].trim()));
+            }
         }
+
+        // Recompensar o minerador
+        String minerador = "Minerador";
+        saldos.put(minerador, saldos.getOrDefault(minerador, 0.0) + novoBloco.getRecompensaMinerador());
     }
 
     public boolean isValida() {
@@ -98,22 +129,28 @@ class Blockchain {
     public Map<String, List<String>> getHistoricoTransacoes() {
         return historicoTransacoes;
     }
+
+    public Map<String, Double> getSaldos() {
+        return saldos;
+    }
+
+    public void resolverFork(List<Bloco> novaCadeia) {
+        if (novaCadeia.size() > blocos.size()) {
+            blocos = novaCadeia;
+            System.out.println("Fork resolvido! Adotando a cadeia mais longa.");
+        }
+    }
 }
 
 class Carteira {
     private String chavePrivada;
     private String chavePublica;
 
-
     public Carteira() {
-
         this.chavePrivada = UUID.randomUUID().toString();
-
-        
         this.chavePublica = sha256(chavePrivada);
     }
 
-    
     private String sha256(String input) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -138,14 +175,11 @@ class Carteira {
         return chavePublica;
     }
 
-
     public String assinarTransacao(String transacao) {
-        
-        return sha256(transacao + chavePrivada); 
-
+        return sha256(transacao + chavePrivada);
+    }
 
     public boolean verificarAssinatura(String transacao, String assinatura) {
-     
         return assinatura.equals(sha256(transacao + chavePrivada));
     }
 }
@@ -154,16 +188,22 @@ class Transacao {
     private String remetente;
     private String destinatario;
     private double valor;
+    private double taxa;
 
     public Transacao(String remetente, String destinatario, double valor) {
         this.remetente = remetente;
         this.destinatario = destinatario;
         this.valor = valor;
+        this.taxa = 0.01; // Taxa fixa de transação
+    }
+
+    public double getTaxa() {
+        return taxa;
     }
 
     @Override
     public String toString() {
-        return remetente + " paga " + destinatario + " " + valor + " BTC";
+        return remetente + ":" + destinatario + ":" + valor;
     }
 }
 
@@ -176,23 +216,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando Blockchain");
 
-
         Carteira carteira = new Carteira();
         System.out.println("Chave pública da carteira: " + carteira.getChavePublica());
         System.out.println("Chave privada da carteira: " + carteira.getChavePrivada());
 
-        
         Transacao t1 = new Transacao("Alice", "Bob", 10);
         Transacao t2 = new Transacao("Bob", "Charlie", 5);
 
-    
         String assinaturaT1 = carteira.assinarTransacao(t1.toString());
         String assinaturaT2 = carteira.assinarTransacao(t2.toString());
 
         System.out.println("Assinatura da transação 1: " + assinaturaT1);
         System.out.println("Assinatura da transação 2: " + assinaturaT2);
 
-    
         boolean verificadoT1 = carteira.verificarAssinatura(t1.toString(), assinaturaT1);
         boolean verificadoT2 = carteira.verificarAssinatura(t2.toString(), assinaturaT2);
         System.out.println("Assinatura da transação 1 verificada: " + verificadoT1);
@@ -200,13 +236,13 @@ public class Main {
 
         Blockchain blockchain = new Blockchain();
         int dificuldade = 4;
+        double recompensaMinerador = 10; // Recompensa para o minerador
 
-    
         List<String> transacoes = new ArrayList<>();
         transacoes.add(t1.toString());
         transacoes.add(t2.toString());
 
-        blockchain.adicionarBloco(transacoes, dificuldade);
+        blockchain.adicionarBloco(transacoes, dificuldade, recompensaMinerador);
 
         System.out.println("Criando bloco com as transações");
         for (Bloco bloco : blockchain.getBlocos()) {
@@ -214,6 +250,7 @@ public class Main {
             System.out.println("  Hash: " + bloco.getHash());
             System.out.println("  Hash Anterior: " + bloco.getHashAnterior());
             System.out.println("  Nonce: " + bloco.nonce);
+            System.out.println("  Recompensa do Minerador: " + bloco.getRecompensaMinerador());
         }
 
         System.out.println("Histórico de transações por endereço:");
@@ -225,5 +262,10 @@ public class Main {
         }
 
         System.out.println("Blockchain é válida: " + blockchain.isValida());
+
+        System.out.println("Saldos:");
+        for (Map.Entry<String, Double> entry : blockchain.getSaldos().entrySet()) {
+            System.out.println("Endereço: " + entry.getKey() + " Saldo: " + entry.getValue() + " BTC");
+        }
     }
 }
